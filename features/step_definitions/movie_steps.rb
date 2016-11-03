5a461de68ba2fc3bcafa22db0fef4ef194dd6fb0
Junaid Ahmed <juahme@ttu.ee>
2016-11-03 22:56:02 +0200

    no message

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,36 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create(movie)
+  end
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  assert page.body =~ /#{e1}.*#{e2}/m
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(", ").each do |rating|
+    if uncheck 
+      uncheck("ratings_#{rating}")
+    else
+      check("ratings_#{rating}")
+    end
+  end
+end
+
+Then /I should see all the movies/ do
+  all_movies = Movie.all
+
+  all_movies.each do |movie|
+    page.should have_content(movie[:title])
+  end
+end
